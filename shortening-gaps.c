539fe1b1423690e21ccf13427896bebde7a07360
Leon Baiyu <leonbaiyu@gmail.com>
2024-01-25 14:04:05 +0000

    Update shortening-gaps.c

--- shortening-gaps.c
+++ shortening-gaps.c
@@ -3,13 +3,24 @@
 main()
 
 {
-    int c, c_prev
-    printf('Give me an input: ');
+    int c;
+    printf("Give me an string of characters including repeated blanks and I will remove them (not including tabs and new lines): ");
 
     /* Receive the input:*/
-    while ( (c = getchar()) != EOF) {
-        /* Check whether character is space*/
-        if c = ' '
+    while (( (c = getchar()) != EOF) ) {
+        /* Check if character is a blank space: ' ' 
+        If character is a blank space, then get next character,
+        repeating the check until the next character is not a blank.
+        Once a non-blank has been detected, output a single blank followed by the detected non-blank*/
+        if (c == ' ') {
+            while (c == ' ') {
+                c = getchar();
+            }
+            printf(" ");
+        }
+        printf("%c",c);
     }
+        /* Output the same character*/
+    
 }
 
